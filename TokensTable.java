88f9bbff174764b4b496ebea28dbebc10847aa57
Salvi <scarrillo@uma.es>
2020-01-27 14:38:39 +0100

    4) Scopes

--- TokensTable.java
+++ TokensTable.java
@@ -8,19 +8,18 @@ public class TokensTable {
 	}
 
 	public Token add(String name, String type) {
+		Token t = null;
 		if(table.containsKey(name)) {
 			error();
+		} else {
+			t = new Token(name, type, null);
+			table.put(name, t);
 		}
-		Token t = new Token(name, type, null);
-		table.put(name, t);
 		return t;
 	}
 
 	public Token extract(String name) {
 		Token t = table.get(name);
-		if(t==null) {
-			error();
-		}
 		return t;
 	}
 }
